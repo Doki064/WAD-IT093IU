14a6a1171bf742792b714ed0b98867462321ebd7
Doki064 <52833936+Doki064@users.noreply.github.com>
2020-12-18 01:37:18 +0700

    Change import to Google style

--- wms/components/__init__.py
+++ wms/components/__init__.py
@@ -1,9 +1,8 @@
-from wms.components import \
-    customer, \
-    item, \
-    item_category, \
-    imports, \
-    import_detail, \
-    shop, \
-    transactions, \
-    transaction_detail
+from wms.components import customer
+from wms.components import import_detail
+from wms.components import imports
+from wms.components import item
+from wms.components import item_category
+from wms.components import shop
+from wms.components import transaction_detail
+from wms.components import transactions
