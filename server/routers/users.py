fcb3c84f3a286255af59559d31e38858469c6f80
Nguyen Nhat Minh <52833936+Doki064@users.noreply.github.com>
2021-06-09 06:27:12 +0700

    Update API users

--- server/routers/users.py
+++ server/routers/users.py
@@ -1,5 +1,5 @@
 """All user API methods."""
-from typing import List, Optional
+from typing import List
 
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
@@ -40,20 +40,22 @@ def create_user(user: UserCreate, db: Session = Depends(get_database)):
     return crud.create(db, user=user)
 
 
-@router.get("/", response_model=User)
-def read_user(user_uid: Optional[int] = None,
-              username: Optional[str] = None,
-              db: Session = Depends(get_database)):
-    db_user = None
-    if user_uid is not None:
-        db_user = crud.get_by_uid()(db, user_uid=user_uid)
-    elif username is not None:
-        db_user = crud.get_by_username(db, username=username)
+@router.get("/{user_uid}", response_model=User)
+def read_user_by_uid(user_uid: int, db: Session = Depends(get_database)):
+    db_user = crud.get_by_uid()(db, user_uid=user_uid)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
 
-@router.get("/all/", response_model=List[User])
+@router.get("/{username}", response_model=User)
+def read_user_by_username(username: str, db: Session = Depends(get_database)):
+    db_user = crud.get_by_username(db, username=username)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@router.get("/", response_model=List[User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_database)):
     return crud.get_all(db, skip=skip, limit=limit)
