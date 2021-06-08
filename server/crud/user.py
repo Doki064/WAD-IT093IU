50bde4edaa2c1219d9047be595e951f5703c0583
Nguyen Nhat Minh <52833936+Doki064@users.noreply.github.com>
2021-06-08 16:14:49 +0700

    Add CRUD methods

--- server/crud/user.py
+++ server/crud/user.py
@@ -15,12 +15,31 @@ def create(db: Session, user: UserCreate):
 
 
 def get_by_uid(db: Session, user_uid: int):
+<<<<<<< HEAD
     return db.query(User).filter(User.id == user_uid).first()
 
 
 def get_by_username(db: Session, username: str):
     return db.query(User).filter(User.email == username).first()
+=======
+    return db.query(User).filter(User.uid == user_uid).first()
+
+
+def get_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+>>>>>>> Add CRUD methods
 
 
 def get_all(db: Session, skip: int = 0, limit: int = 100):
     return db.query(User).offset(skip).limit(limit).all()
+<<<<<<< HEAD
+=======
+
+
+def update_password(db: Session, user_uid: int, hashed_password: str):
+    db_user = get_by_uid(db, user_uid)
+    db_user.hashed_password = hashed_password
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+>>>>>>> Add CRUD methods
