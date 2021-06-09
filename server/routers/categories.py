3b3bd1ac26104ab093e478a9a16acfb18257d425
Nguyen Nhat Minh <52833936+Doki064@users.noreply.github.com>
2021-06-09 22:53:57 +0700

    Update API for items - create item in route of category

--- server/routers/categories.py
+++ server/routers/categories.py
@@ -1,12 +1,14 @@
 """All category API methods."""
 from typing import List
 
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Body
 from sqlalchemy.orm import Session
 
 from database import get_database
-import crud.category as crud
-from schemas import Category, CategoryCreate, Item
+from schemas import Category, CategoryCreate, Item, ItemCreate
+from crud import category as _category
+from crud import shop as _shop
+from crud import item as _item
 
 router = APIRouter(
     prefix="/api/categories",
@@ -19,15 +21,15 @@ router = APIRouter(
 
 @router.post("/", response_model=Category)
 def create_category(category: CategoryCreate, db: Session = Depends(get_database)):
-    db_category = crud.get_by_name(db, name=category.name)
+    db_category = _category.get_by_name(db, name=category.name)
     if db_category is not None:
         raise HTTPException(status_code=400, detail="Category already added")
-    return crud.create(db, category=category)
+    return _category.create(db, category=category)
 
 
 @router.get("/{category_uid}", response_model=Category)
 def read_category(category_uid: int, db: Session = Depends(get_database)):
-    db_category = crud.get_by_uid(db, category_uid=category_uid)
+    db_category = _category.get_by_uid(db, category_uid=category_uid)
     if db_category is None:
         raise HTTPException(status_code=404, detail="Category not found")
     return db_category
@@ -35,7 +37,7 @@ def read_category(category_uid: int, db: Session = Depends(get_database)):
 
 @router.get("/{category_name}", response_model=Category)
 def read_category_by_name(category_name: str, db: Session = Depends(get_database)):
-    db_category = crud.get_by_name(db, name=category_name)
+    db_category = _category.get_by_name(db, name=category_name)
     if db_category is None:
         raise HTTPException(status_code=404, detail="Category not found")
     return db_category
@@ -43,12 +45,26 @@ def read_category_by_name(category_name: str, db: Session = Depends(get_database
 
 @router.get("/", response_model=List[Category])
 def read_categories(skip: int = 0, limit: int = 100, db: Session = Depends(get_database)):
-    return crud.get_all(db, skip=skip, limit=limit)
+    return _category.get_all(db, skip=skip, limit=limit)
+
+
+@router.post("/{category_uid}/items/", response_model=Item)
+def create_item(category_uid: int,
+                item: ItemCreate,
+                shop_name: str = Body(...),
+                db: Session = Depends(get_database)):
+    db_item = _item.get_by_name(db, name=item.name)
+    if db_item is not None:
+        raise HTTPException(status_code=400, detail="Item already added")
+    db_shop = _shop.get_by_name(db, name=shop_name)
+    if db_shop is None:
+        raise HTTPException(status_code=404, detail="Shop not found")
+    return _item.create(db, item=item, category_uid=category_uid, shop_uid=db_shop.uid)
 
 
 @router.get("/{category_uid}/items/", response_model=List[Item])
 def read_category_items(category_uid: int, db: Session = Depends(get_database)):
-    db_category = crud.get_by_uid(db, category_uid=category_uid)
+    db_category = _category.get_by_uid(db, category_uid=category_uid)
     if db_category is None:
         raise HTTPException(status_code=404, detail="Category not found")
     return db_category.items
--- server/routers/items.py
+++ server/routers/items.py
@@ -5,8 +5,8 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 
 from database import get_database
-import crud.item as crud
-from schemas import Item, ItemCreate
+from schemas import Item
+from crud import item as _item
 
 router = APIRouter(
     prefix="/api/items",
@@ -17,20 +17,9 @@ router = APIRouter(
 )
 
 
-@router.post("/", response_model=Item)
-def create_item(item: ItemCreate,
-                category_uid: int,
-                shop_uid: int,
-                db: Session = Depends(get_database)):
-    db_item = crud.get_by_name(db, name=item.name)
-    if db_item is not None:
-        raise HTTPException(status_code=400, detail="Item already added")
-    return crud.create(db, item=item, category_uid=category_uid, shop_uid=shop_uid)
-
-
 @router.get("/{item_uid}", response_model=Item)
 def read_item(item_uid: int, db: Session = Depends(get_database)):
-    db_item = crud.get_by_uid(db, item_uid=item_uid)
+    db_item = _item.get_by_uid(db, item_uid=item_uid)
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return db_item
@@ -38,7 +27,7 @@ def read_item(item_uid: int, db: Session = Depends(get_database)):
 
 @router.get("/{item_name}", response_model=Item)
 def read_item_by_name(item_name: str, db: Session = Depends(get_database)):
-    db_item = crud.get_by_name(db, name=item_name)
+    db_item = _item.get_by_name(db, name=item_name)
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return db_item
@@ -46,4 +35,4 @@ def read_item_by_name(item_name: str, db: Session = Depends(get_database)):
 
 @router.get("/", response_model=List[Item])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_database)):
-    return crud.get_all(db, skip=skip, limit=limit)
+    return _item.get_all(db, skip=skip, limit=limit)
