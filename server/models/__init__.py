129b2ce9597a82bb343133211327cbf8dae32840
Nguyen Nhat Minh <52833936+Doki064@users.noreply.github.com>
2021-06-10 14:00:00 +0700

    Customer name not unique anymore

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -29,7 +29,7 @@ class Customer(Base):
     __tablename__ = "customers"
 
     uid = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), unique=True, index=True, nullable=False)
+    name = Column(String(100), index=True, nullable=False)
 
     transactions = relationship("Transaction", back_populates="customer")
 
