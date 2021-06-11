532263fb0c98dfc07e817e543da94beada5892a5
Nguyen Nhat Minh <52833936+Doki064@users.noreply.github.com>
2021-06-11 14:45:23 +0700

    Update main app

--- server/main.py
+++ server/main.py
@@ -2,20 +2,18 @@ import uvicorn
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
-import database
+from database.config import engine, Base
 import routers
-
-database.create_database()
 
 app = FastAPI()
 
-app.include_router(routers.users_router)
-app.include_router(routers.customers_router)
-app.include_router(routers.categories_router)
-app.include_router(routers.items_router)
-app.include_router(routers.shops_router)
-app.include_router(routers.transactions_router)
-app.include_router(routers.importations_router)
+
+@app.on_event("startup")
+async def startup():
+    # create db tables
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 
 @app.get("/")
@@ -28,5 +26,13 @@ def main():
     return {"message": "Hello, Docker!"}
 
 
+app.include_router(routers.users_router)
+app.include_router(routers.customers_router)
+app.include_router(routers.categories_router)
+app.include_router(routers.items_router)
+app.include_router(routers.shops_router)
+app.include_router(routers.transactions_router)
+app.include_router(routers.importations_router)
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
