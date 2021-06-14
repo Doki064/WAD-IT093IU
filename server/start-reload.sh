8592f8b06e5533b177bf08a7f21d20e88e9822c7
Nguyen Nhat Minh <52833936+Doki064@users.noreply.github.com>
2021-06-15 00:01:54 +0700

    Update for Docker

--- server/start-reload.sh
+++ server/start-reload.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Start uvicorn instead while in development stage
-uvicorn server.main:app --host 0.0.0.0 --port 8080 --reload
+uvicorn main:app --host 0.0.0.0 --port 8080 --reload
