a64e050823060c3965e8de7cf6fc74c4b9b74fae
Nguyen Nhat Minh <52833936+Doki064@users.noreply.github.com>
2021-06-16 00:10:34 +0700

    Fix circular import

---
+++ backend/database/init_db.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+
+from schemas.internal import UserCreate
+from crud import user as _user
+from core.config import settings
+
+
+async def init_db(db: Session):
+    user = await _user.get_by_username(db, username=settings.FIRST_SUPERUSER)
+    if user is None:
+        superuser = UserCreate(username=settings.FIRST_SUPERUSER,
+                               password=settings.FIRST_SUPERUSER_PASSWORD,
+                               is_superuser=True)
+        user = await _user.create(db, user=superuser)
