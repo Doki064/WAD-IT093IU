0572e135874e0f42d504b8963967acce10597caf
Nguyen Nhat Minh <52833936+Doki064@users.noreply.github.com>
2021-06-16 13:29:24 +0700

    Init db with first superuser

--- backend/database/init_db.py
+++ backend/database/init_db.py
@@ -8,7 +8,10 @@ from core.config import settings
 async def init_db(db: Session):
     user = await _user.get_by_username(db, username=settings.FIRST_SUPERUSER)
     if user is None:
-        superuser = UserCreate(username=settings.FIRST_SUPERUSER,
-                               password=settings.FIRST_SUPERUSER_PASSWORD,
-                               is_superuser=True)
+        superuser = UserCreate(
+            username=settings.FIRST_SUPERUSER,
+            password=settings.FIRST_SUPERUSER_PASSWORD,
+            is_active=True,
+            is_superuser=True
+        )
         user = await _user.create(db, user=superuser)
