2327cd51c07a4d1a45d381392c2139db46f93b22
Nguyen Nhat Minh <52833936+Doki064@users.noreply.github.com>
2021-06-18 03:44:08 +0700

    Update prestart.sh

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 # Let the DB start
-python /app/backend_prestart.py
+python /app/backend/backend_prestart.py
 
 # Run migrations
 alembic upgrade head
 
 # Create initial data in DB
-python /app/initial_data.py
+# python /app/backend/initial_data.py
