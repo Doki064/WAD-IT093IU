ee5f79120e7aace6e8e48575205ffbed6a57e7c6
Nguyen Nhat Minh <52833936+Doki064@users.noreply.github.com>
2021-06-18 21:28:29 +0700

    Fix

--- backend/main.py
+++ backend/main.py
@@ -41,7 +41,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origin for origin in settings.BACKEND_CORS_ORIGINS],
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ -n "$SUDO_COMMAND" ]; then
+    echo "Please don't run this script with sudo."
+    exit
+fi
+
+red="\033[0;31m"
+green="\033[0;32m"
+nocolor="\033[0m"
+
+if ! command -v docker-compose >/dev/null; then
+    echo $red "Error: docker-compose needs to be installed" $nocolor
+fi
+
+cd "$(pwd)"
+
+if [ ! -f "$(pwd)/.env" ]; then
+    mv  "$(pwd)/.env.example" "$(pwd)/.env"
+fi
+
+docker-compose build
